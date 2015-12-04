b2de792d0e428c1becda36ad050c60f740bcf930
tl9 <tlim@cs.ucsb.edu>
2015-12-03 20:13:00 -0800

    implement parabolic arc script

---
+++ Assets/Scripts/SphereCollision.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereCollision : MonoBehaviour {
+
+    public float time = 0;
+    public float X = 5;
+    public float Y = 0;
+    public float Z = 0;
+
+	// Use this for initialization
+	void Start () {
+        //transform.position = new Vector3(0, 0, 1);
+        Z = transform.position.z;                
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float t = updateTime();
+        t = t % (X+1);
+        float s = -(2/X) * ((t - X / 2) * (t - X / 2)) + (X / 2);
+
+        transform.position = new Vector3(t, s, Z);
+
+    }
+
+    float trackTime()
+    {
+        return time;
+    }
+
+    float updateTime()
+    {
+        time += (float).1;
+        return time;
+    }
+
+}

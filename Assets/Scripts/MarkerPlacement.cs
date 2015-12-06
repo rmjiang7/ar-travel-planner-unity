0261125436f9c226d50f7863e2999bd94e775f3f
tl9 <tlim@cs.ucsb.edu>
2015-12-06 10:43:59 -0800

    update

--- Assets/Scripts/MarkerPlacement.cs
+++ Assets/Scripts/MarkerPlacement.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MarkerPlacement : MonoBehaviour {
+public class MarkerPlacement : MonoBehaviour
+{
     private float radius_max = 5;
     private float radius = 0;
     private float rad_incr;
+    private float angle_tot = 3 * Mathf.PI;
     private float angle = 0;
     private float angle_incr;
     private float height = 4;
@@ -17,29 +19,32 @@ public class MarkerPlacement : MonoBehaviour {
     private GameObject sphere;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
         rad_incr = radius_max / (num_iter / 2);
-        angle_incr = (2*Mathf.PI) / (num_iter/2);
+        angle_incr = angle_tot / num_iter;
         height_incr = height / num_iter;
-        
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if(height >= 0)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (height >= 0)
             updateParams();
 
-        float x = radius*Mathf.Cos(angle) + X;
+        float x = radius * Mathf.Cos(angle) + X;
         float y = height;
         float z = radius * Mathf.Sin(angle) + Z;
         //transform.position = new Vector3(x, y, z);
         sphere.transform.position = new Vector3(x, y, z);
-	}
+    }
 
-    void updateParams() {
-        if (angle < Mathf.PI * 2)
+    void updateParams()
+    {
+        if (angle < angle_tot / 2)
             radius += rad_incr;
         else
             radius -= rad_incr;

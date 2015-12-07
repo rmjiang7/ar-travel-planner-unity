32416208aa76b062cbf8138eff5598f43fdddec7
tl9 <tlim@cs.ucsb.edu>
2015-12-06 16:02:17 -0800

    added arrowhead

--- Assets/Scripts/ArrowArcs.cs
+++ Assets/Scripts/ArrowArcs.cs
@@ -1,55 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ArrowArcs : MonoBehaviour {
+public class ArrowArcs : MonoBehaviour
+{
     private GameObject cyl;
     public float X = 10;
     public float Y = 0;
     public float Z = 5;
 
-	// Use this for initialization
-	void Start () {
-        int N = 101;
-        float radius = Mathf.Sqrt((X * X) + (Z * Z)) / 2;
-        //Debug.Log("radius=" + radius + " ");
-        float rot_int = (Mathf.PI) / (N - 1);
-        float x_int = X / (N - 1);// (Mathf.Max(X, Z) / 2) / (N - 1);
-        //Debug.Log("xints " + x_int);
-        float z_int = Y / (N - 1);
-        //Debug.Log("zints=" + z_int + " ");
+    private int N = 101;
+    private int max = 0;
 
-        for (int k = 0; k< N; k++)
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float radius = Mathf.Sqrt((X * X) + (Z * Z)) / 2;
+
+        float rot_int = (Mathf.PI) / (N - 1);
+        float x_int = X / (N - 1);
+
+        float z_int = Z / (N - 1);
+
+        if (max <= N)
+        {
+            max++;
+        }
+
+        for (int k = 0; k < max; k++)
         {
             cyl = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-          
+
             float angle = Mathf.PI - (k * rot_int);
-            //Debug.Log("angle: " + angle + " ");
-            //float x = radius * Mathf.Cos(angle) + (X/2);
+
             float x = k * x_int;
             float z = k * z_int;
             float y = radius * Mathf.Sin(angle);
-            //float z = Z;
 
-            //Debug.Log("(x,y)=(" + x + "," + y + ")");
-            cyl.transform.position = new Vector3(x,y,z);
-            cyl.transform.localScale = new Vector3(.3f, .1f, .3f);
+            cyl.transform.position = new Vector3(x, y, z);
+
+            if (k < N - 5)
+            {
+                cyl.transform.localScale = new Vector3(.2f, .1f, .2f);
+            }
+            else  //make last five iters into the arrowhead
+            {
+                float cyl_width = (float)(N - k) / 10;
+                Debug.Log(cyl_width);
+                cyl.transform.localScale = new Vector3(cyl_width, .1f, cyl_width);
+            }
 
             float xRot = k * rot_int;
             float yRot = k * rot_int;
-            float zRot = k * rot_int;
+            float zRot = k * rot_int * (90 / Mathf.PI) * 2;
             cyl.transform.Rotate(0, 0, -zRot);
 
-            //delay(1);
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
-    }
 
-    IEnumerator delay(int sec) {
-        Debug.Log("was here!!");
-        yield return new WaitForSeconds(sec);        
-    }
 }

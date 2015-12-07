313e86592009cc3772aea88074a11b61206f90b0
Richard Jiang <rmjiang7@gmail.com>
2015-12-06 18:14:23 -0800

    Add pulsing to landmarks

--- Assets/Scripts/LandmarkScripts/SelectableCity.cs
+++ Assets/Scripts/LandmarkScripts/SelectableCity.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class SelectableCity : SelectableLandmark {
+	
+	private float time = 0f;
 
 	public Mesh deselectedMesh;
 	public Material deselectedMeshMaterial;
@@ -9,21 +11,42 @@ public class SelectableCity : SelectableLandmark {
 	public Mesh selectedMesh;
 	public Material selectedMeshMaterial;
 
+	public float pulseDuration = 500;
+	public float pulseScaleMultiplier = 1.5f;
+
+	private Vector3 initialScale;
+	private Vector3 finalScale;
+
 	// Use this for initialization
 	protected virtual void Start () {
 		GetComponent<MeshFilter> ().mesh = deselectedMesh;
 		GetComponent<MeshRenderer> ().materials = new Material[]{deselectedMeshMaterial};
 		transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+		time = Time.time * 1000;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (isSelected) {
+			float newTime = Time.time * 1000;
+			Vector3 scaleInt = (finalScale-initialScale) * ((newTime - time) / pulseDuration);
+			transform.localScale = initialScale + scaleInt;
+			if(newTime - time > pulseDuration) {
+				time = newTime;
+				Vector3 temp = initialScale;
+				initialScale = finalScale;
+				finalScale = temp;
+			}
+		}
 	}
 
 	public override void OnSelect() {
 		GetComponent<MeshFilter> ().mesh = selectedMesh;
 		GetComponent<MeshRenderer> ().materials = new Material[]{selectedMeshMaterial};
 		transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+		initialScale = transform.localScale;
+		finalScale = transform.localScale * pulseScaleMultiplier;
+		time = Time.time * 1000;
 	}
 
 	public override void OnDeselect() {
--- Assets/Scripts/LandmarkScripts/TokyoSelectableCity.cs
+++ Assets/Scripts/LandmarkScripts/TokyoSelectableCity.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class TokyoSelectableCity : SelectableCity {
-	
+
 	private GameObject uiInfoPanel;
 	// Update is called once per frame
 	protected override void Start () {
@@ -13,6 +13,7 @@ public class TokyoSelectableCity : SelectableCity {
 	public override void OnSelect() {
 		base.OnSelect ();
 		uiInfoPanel.GetComponent<CanvasGroup>().alpha = 1.0f;
+
 	}
 
 	public override void OnDeselect() {
